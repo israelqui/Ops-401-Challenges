ea3aea9f7f647e69fe21cc3ef971abcd00bc9454
israel <quirolaisraela@gmail.com>
2024-03-05 21:44:00 -0500

    Attack Tools Part 2 of 3

---
+++ challenge42.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Script Name: Attack Tools Part 2 of 3
+# Description: Using Python to develop a custom Nmap scanner that can later be combined with other Python scripts to create a pentester toolkit.
+# Author: Israel Quirola
+# Date: February 26, 2024
+
+import nmap
+import ipaddress
+
+scanner = nmap.PortScanner()
+
+print("Nmap Automation Tool")
+print("--------------------")
+
+ip_addr = input("IP address to scan: ")
+print("The IP you entered is: ", ip_addr)
+
+# Validate IP address format
+try:
+    ip_address = ipaddress.ip_address(ip_addr)
+except ValueError:
+    print("Invalid IP address format.")
+    exit()
+
+resp = input("""\nSelect scan to execute:
+                1) SYN ACK Scan
+                2) UDP Scan
+                3) Full TCP Connect Scan\n""")
+
+print("You have selected option: ", resp)
+
+port_range = input("Enter port range (e.g., 1-100): ")
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, port_range, '-v -sS')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, port_range, '-v -sU')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, port_range, '-v -sT')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+else:
+    print("Please enter a valid option.")
